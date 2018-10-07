4d283e9b0d9981ecce805a1ee0d7475abfd00e60
Patrik Johnson <patrik@tellybean.com>
2018-10-07 14:59:07 +0300

    Initial dev setup with rollup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import rootImport  from 'rollup-plugin-root-import'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs    from 'rollup-plugin-commonjs'
+import copy        from 'rollup-plugin-copy'
+import buble       from 'rollup-plugin-buble'
+import filesize    from 'rollup-plugin-filesize'
+
+module.exports = {
+  input   : 'src/app.js',
+  output  : {
+    file: 'dist/app.js',
+    format  : 'umd',
+  },
+  plugins : [
+    rootImport({ root: `${__dirname}/src`, extensions: ['.js', '/index.js'] }),
+    nodeResolve({ jsnext: true, main: true, browser: true, preferBuiltins: false }),
+    commonjs({}),
+    copy({ 'resources/' : 'dist/' }),
+    buble(),
+    filesize()
+  ]
+}
